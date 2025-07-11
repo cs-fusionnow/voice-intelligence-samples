ad225852de344eef6cb85c9f4fc3321d5eae54f4
cs-fusionnow <144822154+cs-fusionnow@users.noreply.github.com>
2025-07-11 15:43:41 -0500

    Updated service ID

--- transcripts-integration-demo/api/transcripts-embedded.js
+++ transcripts-integration-demo/api/transcripts-embedded.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const transcriptViewerEmbeddedUrl = process.env.TRANSCRIPT_VIEWER_EMBEDDED_URL;
   const transcriptsAssetUrl = process.env.TRANSCRIPTS_ASSET_URL;
 
-  const viewLink = encodeURIComponent(`${transcriptViewerEmbeddedUrl}?serviceSid=RE91e7dfed2cb7b0ea06da85ed9c33f55c&transcriptSid=GT5b2a576526d230d217af879c9ad5fd07`);
+  const viewLink = encodeURIComponent(`${transcriptViewerEmbeddedUrl}?serviceSid=GA71776b2c22c2f6821e2044256d77f3ca&transcriptSid=GT5b2a576526d230d217af879c9ad5fd07`);
   const token = await client.getTranscriptsToken(viewLink);
   const iframeUrl = `${transcriptsAssetUrl}?token=${token}`;
 
