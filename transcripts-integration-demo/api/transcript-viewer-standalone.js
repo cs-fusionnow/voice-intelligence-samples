7569a6c2c76c0611221e5c4d004e05caed44c18d
cs-fusionnow <144822154+cs-fusionnow@users.noreply.github.com>
2025-07-11 16:41:46 -0500

    reverted

--- transcripts-integration-demo/api/transcript-viewer-standalone.js
+++ transcripts-integration-demo/api/transcript-viewer-standalone.js
@@ -2,19 +2,13 @@ const client = require('./twilioClient');
 
 export default async function handler(req, res) {
   const { serviceSid, transcriptSid } = req.query;
-  const iframeBaseUrl = process.env.TRANSCRIPTS_ASSET_URL;
-  const appBaseUrl = process.env.APP_BASE_URL; // your deployed app domain, e.g. https://your-app.vercel.app
 
   if (serviceSid && transcriptSid) {
-    const viewLink = `${appBaseUrl}/api/transcript-viewer-standalone?serviceSid=${serviceSid}&transcriptSid=${transcriptSid}`;
     const transcriptViewerAssetUrl = process.env.TRANSCRIPT_VIEWER_ASSET_URL;
-    const token = await client.getTranscriptsTokenFromViewLink(viewLink); // const token = await client.getTranscriptViewerToken(serviceSid, transcriptSid);
-    const iframeUrl = `${iframeBaseUrl}?token=${token}`;
+    const token = await client.getTranscriptViewerToken(serviceSid, transcriptSid);
+    const url = `${transcriptViewerAssetUrl}?token=${token}`;
 
-    res.status(200).json({ iframeUrl });
-    //const url = `${transcriptViewerAssetUrl}?token=${token}`;
-
-    //return res.writeHead(302, { Location: url }).end();
+    return res.writeHead(302, { Location: url }).end();
   }
 
   // Basic fallback for errors (can customize)
