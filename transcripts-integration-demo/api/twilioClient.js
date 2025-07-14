58a0f2cc9796d7e47f73ea459aaba183c85e3829
cs-fusionnow <144822154+cs-fusionnow@users.noreply.github.com>
2025-07-14 13:11:14 -0500

    Update twilioClient.js

--- transcripts-integration-demo/api/twilioClient.js
+++ transcripts-integration-demo/api/twilioClient.js
@@ -6,71 +6,63 @@ const twilioApiSecret = process.env.TWILIO_API_SECRET;
 const twilioEncodedCreds = Buffer.from(`${twilioApiKey}:${twilioApiSecret}`).toString('base64');
 
 module.exports = {
-    getTranscriptsToken: async (viewLink) => {
-        const rsp = await fetch(
-            'https://ai.twilio.com/v1/Tokens',
-            {
-                method: 'POST',
-                headers: {
-                    Authorization: `Basic ${twilioEncodedCreds}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(
-                    {
-                        grants: [{
-                            product: 'intelligence-discovery',
-                            metadata: {
-                                viewLinks: {
-                                    conversationViewLink: {
-                                        target: "_top",
-                                        href: viewLink
-                                    }
-                                }
-                            }
-                        }]
-                    }
-                )
-            }
-        );
+    getTranscriptsTokenWithViewerRedirect: async () => {
+    const viewLinkTemplate = `${process.env.APP_BASE_URL}/api/transcript-viewer-standalone?serviceSid=:serviceSid&transcriptSid=:transcriptSid`;
 
-        if (rsp.ok) {
-            const { token } = await rsp.json();
-            return token;
-        } else {
-            throw new Error('failed to generate token');
-        }
-    },
-    getTranscriptViewerToken: async (serviceSid, transcriptSid) => {
-        const rsp = await fetch(
-            'https://ai.twilio.com/v1/Tokens',
-            {
-                method: 'POST',
-                headers: {
-                    Authorization: `Basic ${twilioEncodedCreds}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(
-                    {
-                        grants: [{
-                            product: 'annotator',
-                            service_sid: serviceSid,
-                            transcript_sid: transcriptSid,
-                            metadata: {}
-                        }]
-                    }
-                )
+    const rsp = await fetch('https://ai.twilio.com/v1/Tokens', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${twilioEncodedCreds}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        grants: [{
+          product: 'intelligence-discovery',
+          metadata: {
+            viewLinks: {
+              conversationViewLink: {
+                target: "_top",
+                href: viewLinkTemplate
+              }
             }
-        );
+          }
+        }]
+      })
+    });
 
-        if (rsp.ok) {
-            const { token } = await rsp.json();
-            return token;
-        } else {
-            throw new Error('failed to generate token');
-        }
+    if (rsp.ok) {
+      const { token } = await rsp.json();
+      return token;
+    } else {
+      throw new Error('Failed to generate discovery token with viewer redirect');
     }
-};
+  },
 
+  getTranscriptViewerToken: async (serviceSid, transcriptSid) => {
+    const rsp = await fetch('https://ai.twilio.com/v1/Tokens', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${twilioEncodedCreds}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        grants: [{
+          product: 'annotator',
+          service_sid: serviceSid,
+          transcript_sid: transcriptSid,
+          metadata: {}
+        }]
+      })
+    });
+
+    if (rsp.ok) {
+      const { token } = await rsp.json();
+      return token;
+    } else {
+      throw new Error('Failed to generate viewer token');
+    }
+  }
+};
 getTranscriptsTokenFromViewLink: async (viewLink) => {
   const rsp = await fetch('https://ai.twilio.com/v1/Tokens', {
     method: 'POST',
