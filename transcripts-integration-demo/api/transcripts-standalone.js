bf14f74f3eeaa09aa82ef1d15751d4ed599d9ea2
cs-fusionnow <144822154+cs-fusionnow@users.noreply.github.com>
2025-07-14 12:34:02 -0500

    Removed specific transcript from standalone

--- transcripts-integration-demo/api/transcripts-standalone.js
+++ transcripts-integration-demo/api/transcripts-standalone.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const transcriptViewerEmbeddedUrl = process.env.TRANSCRIPT_VIEWER_EMBEDDED_URL;
   const transcriptsAssetUrl = process.env.TRANSCRIPTS_ASSET_URL;
 
-  const viewLink = encodeURIComponent(`${transcriptViewerEmbeddedUrl}?serviceSid=GA71776b2c22c2f6821e2044256d77f3ca&transcriptSid=GT5b2a576526d230d217af879c9ad5fd07`);
+  const viewLink = encodeURIComponent(`${transcriptViewerEmbeddedUrl}`);
   const token = await client.getTranscriptsToken(viewLink);
   const url = `${transcriptsAssetUrl}?token=${token}`;
 
